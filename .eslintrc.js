12142e4a8ee932344b04b5a995146955106b5373
Rijk van Zanten <rijkvanzanten@me.com>
2018-02-16 19:22:25 -0500

    Add dotfiles (#2)
    * Add editorconfig
    
    * Add postcss config
    
    * Add eslint rules
    
    * Add babel config

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // ignore state in no-param-reassign
+    'no-param-reassign': ['error', {
+      ignorePropertyModificationsFor: [
+        'state' // Vuex
+      ]
+    }],
+    'import/no-extraneous-dependencies': 'warning'
+  }
+}
