f970644a328a6ae847744d7b7622e3e1966dac4d
Rijk van Zanten <rijkvanzanten@me.com>
2018-04-02 16:09:44 -0400

    Add v-card component (#79)
    * Add v-card component markup
    
    * Register v-card as a global component
    
    * Add accent box shadow variable
    
    * Style card component
    
    * Add label

---
+++ src/components/VCard.vue
@@ -0,0 +1,153 @@
+<template>
+  <component
+    :is="element"
+    :class="href ? 'link' : null"
+    class="v-card">
+    <component
+      :is="wrapperTag"
+      :href="href">
+      <div
+        v-if="src || icon"
+        class="header">
+        <img
+          v-if="src"
+          :alt="title"
+          :src="src">
+        <i
+          v-if="icon"
+          class="material-icons">{{ icon }}</i>
+        <span
+          v-if="label"
+          class="label">{{ label }}</span>
+      </div>
+      <div class="body">
+        <component
+          :is="titleElement"
+          class="title "
+        >{{ title }}</component>
+        <p class="subtitle style-4">{{ subtitle }}</p>
+      </div>
+    </component>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'v-card',
+  props: {
+    element: {
+      type: String,
+      default: 'article',
+    },
+    titleElement: {
+      type: String,
+      default: 'h2',
+    },
+    icon: {
+      type: String,
+      default: null,
+    },
+    src: {
+      type: String,
+      default: null,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      default: null,
+    },
+    href: {
+      type: String,
+      default: null,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    wrapperTag() {
+      return this.href ? 'a' : 'div';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-card {
+  width: 160px;
+  border-radius: 3px;
+  box-shadow: var(--box-shadow);
+  overflow: hidden;
+  transition: box-shadow var(--fast) var(--transition);
+
+  a {
+    text-decoration: none;
+  }
+
+  &.link:hover {
+    box-shadow: var(--box-shadow-accent);
+  }
+
+  .header {
+    height: 160px;
+    display: grid;
+    grid-template-columns: 1;
+    grid-template-rows: 1;
+    background-color: var(--primary);
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    i {
+      font-size: 64px;
+      color: var(--white);
+      text-align: center;
+    }
+
+    img, i {
+      grid-row: 1;
+      grid-column: 1;
+    }
+
+    .label {
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+      padding: 2px 5px;
+      border-radius: var(--border-radius);
+      opacity: 0.5;
+      background-color: var(--white);
+      color: var(--darker-gray);
+      backdrop-filter: blur(5px);
+      font-size: 10px;
+      text-transform: uppercase;
+    }
+  }
+
+  .body {
+    padding: 10px;
+  }
+
+  .title,
+  .subtitle {
+    width: 100%;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  .title {
+    margin-bottom: 5px;
+  }
+}
+</style>
