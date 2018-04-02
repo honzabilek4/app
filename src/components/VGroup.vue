cd0b39a50f586a08b1ffe527470c66bccd3ba4cc
Rijk van Zanten <rijkvanzanten@me.com>
2018-04-02 14:55:22 -0400

    Add v-group component (#78)
    * [WIP] Start on v-group component
    
    * Add v-group to the globals
    
    * Style summary
    
    * Add margin to top and bottom of details

---
+++ src/components/VGroup.vue
@@ -0,0 +1,77 @@
+<template>
+  <details
+    :open="open"
+    class="v-group">
+    <summary>{{ title }}</summary>
+    <slot />
+  </details>
+</template>
+
+<script>
+export default {
+  name: 'v-group',
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+    open: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-group {
+  width: 100%;
+  margin: 10px 0;
+
+  summary {
+    border-top: 1px solid var(--lighter-gray);
+    border-bottom: 1px solid var(--lighter-gray);
+    padding: 10px 0;
+    margin-bottom: 10px;
+    font-size: 18px;
+    color: var(--darker-gray);
+    list-style-type: none;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+
+    &::after {
+      content: 'keyboard_arrow_left';
+      direction: ltr;
+      display: inline-block;
+      font-family: 'Material Icons';
+      font-size: 24px;
+      font-style: normal;
+      font-weight: normal;
+      letter-spacing: normal;
+      line-height: 1;
+      text-transform: none;
+      white-space: nowrap;
+      word-wrap: normal;
+      -webkit-font-feature-settings: 'liga';
+      -webkit-font-smoothing: antialiased;
+      transition: var(--fast) var(--transition);
+    }
+
+    &::-webkit-details-marker {
+      display: none;
+    }
+
+    &:hover,
+    .user-is-tabbing &:focus {
+      color: var(--darkest-gray);
+    }
+  }
+
+  &[open] summary::after {
+    transform: rotate(-90deg);
+  }
+}
+</style>
