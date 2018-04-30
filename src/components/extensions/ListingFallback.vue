21e0adfa97ad5b4e7430e2e7d2c168e8c1101a3a
Rijk van Zanten <rijkvanzanten@me.com>
2018-04-30 18:21:40 -0400

    Add fallback listing (#315)

--- src/components/extensions/ListingFallback.vue
+++ src/components/extensions/ListingFallback.vue
@@ -1,9 +1,36 @@
 <template>
-  <div class="listing-fallback" />
+  <v-table
+    v-if="items && columns && primaryKeyField"
+    :items="items"
+    :columns="columns"
+    :primaryKeyField="primaryKeyField"
+    link="__link__"
+    :use-interfaces="true" />
 </template>
 
 <script>
 export default {
-  name: "listing-fallback"
+  name: "listing-fallback",
+  props: {
+    fields: {
+      type: Object,
+      required: true
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    primaryKeyField: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    columns() {
+      return Object.values(this.fields).map(field => {
+        return { field: field.field, name: field.name, fieldInfo: field };
+      });
+    }
+  }
 };
 </script>
