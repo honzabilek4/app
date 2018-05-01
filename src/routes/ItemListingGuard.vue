3500d10c13588a34de0e7098101fb7f25b6f185b
Rijk van Zanten <rijkvanzanten@me.com>
2018-05-01 12:30:08 -0400

    Add support for adding single page collections (#332)
    * Upgrade npm dependencies
    
    * Add support for single item collections
    
    To be fair, the implementation is butt-ugly, but seeing refactoring
    takes too long for this week, the "final" implementation of this needs
    to wait a little bit 🙃

--- src/routes/ItemListingGuard.vue
+++ src/routes/ItemListingGuard.vue
@@ -1,17 +1,26 @@
 <template>
   <component
+    v-if="component === 'edit' && !loading"
+    :is="component"
+    :primary-key="primaryKey"
+    :collection="collection" />
+  <div v-else-if="loading" />
+  <component
+    v-else
     :is="component"
     :collection="collection" />
 </template>
 
 <script>
 import ItemListing from "./ItemListing.vue";
+import Edit from "./Edit.vue";
 import NotFound from "./NotFound.vue";
 
 export default {
   name: "item-listing-guard",
   components: {
     ItemListing,
+    Edit,
     NotFound
   },
   props: {
@@ -20,14 +29,81 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      loading: false
+    };
+  },
   computed: {
     collections() {
-      return Object.keys(this.$store.state.collections.data);
+      return this.$store.state.collections.data;
+    },
+    collectionNames() {
+      return Object.keys(this.collections);
     },
     component() {
-      return this.collections.includes(this.collection)
-        ? "item-listing"
-        : "not-found";
+      const collectionExists = this.collectionNames.includes(this.collection);
+
+      if (collectionExists === false) return "not-found";
+
+      const collectionInfo = this.collections[this.collection];
+
+      if (collectionInfo.single === true) return "edit";
+
+      return "item-listing";
+    },
+    fields() {
+      const stateFields =
+        (this.$store.state.fields &&
+          this.$store.state.fields[this.collection] &&
+          this.$store.state.fields[this.collection].data) ||
+        {};
+
+      return this.$lodash.keyBy(
+        Object.values(stateFields)
+          .filter(field => {
+            if (field.interface === "primary-key") return true;
+
+            if (field.hidden_list === true || field.hidden_list === 1) {
+              return false;
+            }
+
+            return true;
+          })
+          .map(field => ({
+            ...field,
+            name: this.$t(`fields-${this.collection}-${field.field}`)
+          })),
+        "field"
+      );
+    },
+    primaryKeyField() {
+      const primaryKeyField = this.$lodash.find(
+        this.fields,
+        { interface: "primary-key" } // TODO: check for data type instead of interface
+      );
+
+      return primaryKeyField && primaryKeyField.field;
+    }
+  },
+  created() {
+    if (this.component === "edit") {
+      this.loading = true;
+
+      this.$store
+        .dispatch("getFields", this.collection)
+        .then(() => {
+          return this.$api.getItems(this.collection, {
+            limit: 1,
+            fields: [this.primaryKeyField]
+          });
+        })
+        .then(res => res.data)
+        .then(data => {
+          this.primaryKey = String(data[0][this.primaryKeyField]);
+          this.loading = false;
+        })
+        .catch(console.error);
     }
   }
 };
